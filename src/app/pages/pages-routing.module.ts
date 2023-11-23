2bd165f69e73d8de2b848fb81d41dfc4fde14a99
karla_rios <riosdiazkm@gmail.com>
2023-11-23 17:14:46 -0600

    Avance servicio autenticación

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,16 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './template/home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-
- /* { path: 'about', component: AboutComponent, data: { title: 'Sobre mí' } },
-  { path: 'skills', component: SkillsComponent, data: { title: 'Skills' } },
-  { path: 'projects', component: ProjectsComponent, data: { title: 'Proyectos' } },
-  { path: 'contact', component: ContactComponent, data: { title: 'Contáctame' } },
-*/
-
   { path: '**', redirectTo: '' },
 ];
 @NgModule({
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { tap } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(private http: HttpClient) {}
+
+  login(credentials: { email: string; password: string }) {
+    return this.http.post(`${environment.SERVER_URL}/login`, credentials).pipe(
+      tap((response: any) => {
+        if (response && response.token) {
+          // Guardar el token en el localStorage
+          localStorage.setItem('token', response.token);
+        }
+      })
+    );
+  }
+
+  logout() {
+    return this.http.post(`${environment.SERVER_URL}/logout`, {});
+  }
+
+  isAuthenticated(): boolean {
+    return localStorage.getItem('token') !== null;
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem('token');
+  }
+}
