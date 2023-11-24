bf95e772840c17b6ca78a0c82889764fcef2b47c
karla_rios <riosdiazkm@gmail.com>
2023-11-23 18:02:56 -0600

    Corrección rutas auth

--- src/app/pages/auth/auth-routing.module.ts
+++ src/app/pages/auth/auth-routing.module.ts
@@ -2,15 +2,26 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { TemplateComponent } from './template/template.component';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent },
-  { path: 'login', component: LoginComponent, data: { title: 'Login' } },
-  { path: 'register', component: RegisterComponent, data: { title: 'Registro' } },
-  { path: '**', redirectTo: '' },
+  {
+    path: '',
+    component: TemplateComponent,
+    children: [
+      { path: '', component: LoginComponent },
+      { path: 'login', component: LoginComponent, data: { title: 'Login' } },
+      {
+        path: 'register',
+        component: RegisterComponent,
+        data: { title: 'Registro' },
+      },
+      { path: '**', redirectTo: '' },
+    ],
+  },
 ];
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AuthRoutingModule { }
+export class AuthRoutingModule {}
